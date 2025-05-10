294b60c19925698c35af2abd0934960eef39dfb6
CamouflageNetwork <notmrmacattax@gmail.com>
2025-05-09 20:53:19 -0400

    Update proxyDependencies.js

--- js/proxyDependencies.js
+++ js/proxyDependencies.js
@@ -1,3 +1,4 @@
-let proxyUrl = `https://camo-api-v2.vercel.app/api/index.js?url=`;
+let proxyUrl = `/api/index.js?url=`; // Change this to your proxy url being hosted if you have a seperate URL
+let eruda = true;  // true = Dev Console, Anything else = No Dev Console
 
-export { proxyUrl };
+export { proxyUrl, eruda };

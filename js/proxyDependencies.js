3c8991e2bac8f0dfd87c03ca2c80d3a71839c2e5
CamouflageNetwork <notmrmacattax@gmail.com>
2025-05-10 21:29:53 -0400

    Update proxyDependencies.js

--- js/proxyDependencies.js
+++ js/proxyDependencies.js
@@ -1,4 +1,3 @@
-let proxyUrl = `/api/index.js?url=`; // Change this to your proxy url being hosted if you have a seperate URL
-let eruda = true;  // true = Dev Console, Anything else = No Dev Console
+const eruda = true;  // true = Dev Console, Anything else = No Dev Console
 
 export { proxyUrl, eruda };

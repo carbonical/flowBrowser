9e2e4ded001f8b3e5db703e9af77209ae3b887e7
CamouflageNetwork <notmrmacattax@gmail.com>
2025-05-11 17:41:15 -0400

    Update proxyDependencies.js

--- js/proxyDependencies.js
+++ js/proxyDependencies.js
@@ -1,3 +1,9 @@
-const eruda = true;  // true = Dev Console, Anything else = No Dev Console
+const fs = require('fs');
+const path = require('path');
+
+const settingsPath = path.join(__dirname, 'settings.json');
+const settings = JSON.parse(fs.readFileSync(settingsPath, 'utf8'));
+
+const eruda = settings.eruda?.enabled || false;
 
 module.exports = { eruda };
